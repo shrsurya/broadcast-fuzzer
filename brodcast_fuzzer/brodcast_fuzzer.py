4708e832a666259630e95a89b0a966b505f518db
Shridhar Suryanarayan <s4suryan@uwaterloo.ca>
2022-06-28 15:50:21 -0400

    added structure for main object:

--- brodcast_fuzzer/brodcast_fuzzer.py
+++ brodcast_fuzzer/brodcast_fuzzer.py
@@ -10,3 +10,35 @@ class BrodcastFuzzer(object):
     def __init__(self, **kwargs) -> None:
         """Configure self and execute"""
         pass
+
+    def extract_apk(self, apk_path, extract_folder):
+        """
+        Use apktool to extract apk file
+        apk_path: absolute path the apk file
+        extract_folder:
+        apktool d -s yourapk.apk -o yourfolder 
+
+        Assumes: user has apktool installed
+        """
+        pass
+
+    def extract_xml(self):
+        """
+        <manifest>
+            package
+            <application>
+                list of [<service> // or <activity> or <receiver>
+                    android:name
+                    <intent-filter>
+                        <action>
+                            android:name
+                        </action>
+                        <data>
+                            android:mimeType
+                        </data>
+                    <intent-filter>
+                </service> // or </activity> or </receiver> ]
+            </application>
+        </maifest>
+        """
+        pass
